c8652749cf0c54f9f3832fedd3ecfd6307a9f8ee
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-11-15 19:08:51 +0700

    simple hello word + fix mistype

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-routes(app)
+app.use(routes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
+app.listen(process.env.SERVER_PORT, () => {
+  serverHostAndPort = `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
+  message = `Server is running on ${serverHostAndPort}`
+  console.log(message)
 })
